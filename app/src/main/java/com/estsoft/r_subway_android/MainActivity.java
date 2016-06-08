b5648ae86aea9f72542c6007576267c875da7bf7
Gong Ui Jang <dalkomsft02@gmail.com>
2016-06-09 00:19:12 +0900

    init
     화이팅

---
+++ app/src/main/java/com/estsoft/r_subway_android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.estsoft.r_subway_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
